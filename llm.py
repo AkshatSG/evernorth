ad6700b33110202d21e6c4b66bffe7001a297278
Akshat Santhana Gopalan <akshatsg12@outlook.com>
2024-07-13 15:05:36 -0400

    Update llm.py

--- llm.py
+++ llm.py
@@ -3,3 +3,21 @@
 #LLM returns our desired data needs
 
 #Still in planning phase
+
+import os
+import requests
+
+def generate_answer(context, question, model='distilbert-base-uncased-distilled-squad'): #You can change the model to your needs
+    api_key='Your HF API KEY' #Ought to convert this to an env var
+    headers={"Authorization": f"Bearer {api_key}"}
+    url=f"https://api-inference.huggingface.co/models/{model}"
+
+    payload={
+        "inputs": {
+            "question": question,
+            "context": context
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    return response.json()
